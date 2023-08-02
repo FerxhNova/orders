57446287c48f00e3d083703bb5471682a1170328
ferxhnova <ferxh77@gmail.com>
2023-08-02 16:17:20 -0600

    POST orders

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/orders", (req, res) => {
+  const requestBody = req.body;
+  res.json(requestBody);
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`App listening on port ${port}`);
+});
