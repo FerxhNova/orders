dfb5da3167537612f7b5e701b6475824abff0b6c
ferxhnova <ferxh77@gmail.com>
2023-08-02 16:32:06 -0600

    express json

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.use(express.static("public"));
-
+app.use(express.json());
 app.get("*", (req, res) => {
   res.redirect("/");
 });
 
 app.post("/orders", (req, res) => {
   const requestBody = req.body;
-  console.log(requestBody);
   res.json(requestBody);
 });
 
